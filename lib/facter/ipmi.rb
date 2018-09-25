29229be64e37a1da7f5af5bb68070760949cc61d
Mykhailo Danylenko <mikhail.danylenko@uvoteam.com>
2018-09-25 15:45:19 +0300

    fixup! fixup! fixup! fixup! fixup! Add parameter compatibility with pre-rewrite module

--- lib/facter/ipmi.rb
+++ lib/facter/ipmi.rb
@@ -1,20 +1,20 @@
 # facts for compatibility with older version of this module.
 # XXX this does not currently implement 'ipaddress_source' and 'macaddress' facts.
 
-require File.join(File.dirname(__FILE__), '..', '..', '..', 'puppet_x', 'ipmi')
+require File.join(File.dirname(__FILE__), '..', 'puppet_x', 'ipmi')
 
 IPMI.lan_channels.map do |lan|
-    Facter.add("ipmi#{lan.channel}_ipaddress") do
+    Facter.add("ipmi#{lan.cid}_ipaddress") do
         setcode do
             lan.ipaddr
         end
     end
-    Facter.add("ipmi#{lan.channel}_subnet_mask") do
+    Facter.add("ipmi#{lan.cid}_subnet_mask") do
         setcode do
             lan.netmask
         end
     end
-    Facter.add("ipmi#{lan.channel}_gateway") do
+    Facter.add("ipmi#{lan.cid}_gateway") do
         setcode do
             lan.defgw_ipaddr
         end
