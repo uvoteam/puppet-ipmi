df767940ccb9c83fb60e87df50c24f5264524847
Mykhailo Danylenko <isbear@isbear.org.ua>
2022-02-25 23:17:26 +0200

    Make random passwords adhere to new supermicro requirements
    It seems, that supermicro now requires, that password have to be 8+
    chars long, have at least one uppercase letter and at least one digit.

--- lib/puppet_x/random_password.rb
+++ lib/puppet_x/random_password.rb
@@ -2,7 +2,7 @@
 # Just a helper function
 module HelperRandomPassword
     def self.random_password length = 15
-        ('a'..'z').to_a.shuffle[0,length].join
+        (('A'..'Z').to_a.shuffle[0,1] + ('a'..'z').to_a.shuffle[0,length-2] + ('0'..'9').to_a.shuffle[0,1]).shuffle.join
     end
 end
 
